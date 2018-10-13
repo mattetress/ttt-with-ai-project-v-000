1948fbf9e7b8fe65e0b90a0adecee574ba8187ce
Matt Etress <mattetress@me.com>
2018-10-13 01:54:18 +0000

    automatically backed up by learn

--- lib/game_flow_controller.rb
+++ lib/game_flow_controller.rb
@@ -1,3 +1,17 @@
 class GameFlowController
 
+  def start
+    puts "Hi! Welcome to Tic Tac Toe!"
+    puts "What type of game would you like to play?"
+    puts "1. 0 Player"
+    puts "2. 1 Player"
+    puts "3. 2 Player"
+    puts "Please enter your selection:"
+    input = gets.chomp
+    input
+  end
+
+  def zero_player
+    game = Game.new(Players::Computer("X"), Players::Computer("O"), Board.new)
+
 end
