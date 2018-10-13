d300a08a528aa44823bc2b34557e5aaa4b2b38bb
Matt Etress <mattetress@me.com>
2018-10-13 00:56:34 +0000

    automatically backed up by learn

--- lib/players/computer.rb
+++ lib/players/computer.rb
@@ -9,14 +9,21 @@ module Players
     @opponents_spaces = []
     @my_spaces = []
 
+    close_to_winning = [
+      [1, 2], [2, 3], [4, 5], [5, 6],
+      [7, 8], [8, 9], [1, 4], [4, 7],
+      [2, 5], [5, 8], [3, 6], [6, 9],
+      [1, 5], [5, 9], [3, 5], [5, 7]
+    ]
+
     def view_board
       i = 1
       until i = 9 do
-        if board.taken?(i) 
+        if board.taken?(i)
           if board.cells[i - 1] == self.token
-            my_spaces << i 
-          else 
-            opponents_spaces << i 
+            my_spaces << i
+          else
+            opponents_spaces << i
           end
         end
       end
@@ -35,6 +42,14 @@ module Players
           choose(CENTER)
         when 1 && board.taken?(CENTER)
           choose(CONRERS.sample)
+        when 2 && my_spaces == [5] && opponents_spaces == [2] 
+          choose([7, 9].sample)
+        when 2 && my_spaces == [5] && opponents_spaces == [4]
+          choose([3, 9].sample)
+        when 2 && my_spaces == [5] && opponents_spaces == [6]
+          choose([1, 7].sample)
+        when 2 && my_spaces == [5] && opponents_spaces == [8]
+          choose([1, 3].sample)
       end
     end
 
